a43e08afa8c5309e27f354b29c81abc82a40b735
Dale Stanford <dale.stanford350@outlook.com>
2021-05-26 17:14:22 -0500

    Add project files.

---
+++ MyFirstSignalR/MyHub1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//
+// This is server side code!!
+//
+
+namespace MyFirstSignalR
+{
+    // The HubName attributes enable a custom hub name without using Pascal case
+    // This attribute is not required.
+    [HubName("myHub")]
+    public class MyHub : Hub
+    {
+        public void Hello(string message)
+        //public void Send(string message)
+        {
+            Clients.All.hello(message);
+            //Clients.All.broadcastMessage(message);
+        }
+    }
+}
